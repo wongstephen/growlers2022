c368eb97ebc024703ed3a557d1ff03a5d2c8d0a9
Stephen Wong <wongstephenky@gmail.com>
2023-12-03 23:10:02 -0800

    refactor: remove docs

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,14 +1,3 @@
-/**
- * Renders a service card component.
- *
- * @param {Object} props - The component props.
- * @param {string} props.title - The title of the service.
- * @param {string} props.description - The description of the service.
- * @param {React.Ref} props.ref - The ref for the component.
- * @param {React.ReactNode} props.btnComponent - The button component to render.
- * @returns {JSX.Element} The rendered service card component.
- */
-
 import clsx from "clsx";
 
 export default function ServiceCard({
